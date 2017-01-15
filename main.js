a1cc9b981acafce1c1b752d3da798bc49ce56ab7
ca333 <ca333@safe-mail.net>
2017-01-15 14:11:57 +0100

    init

---
+++ main.js
@@ -0,0 +1,60 @@
+const electron = require('electron')
+
+const app = electron.app
+
+const BrowserWindow = electron.BrowserWindow
+const fixPath = require('fix-path');
+const pm2 = require('pm2');
+const path = require('path')
+const url = require('url')
+const execFile = require('child_process').execFile;
+fs = require('fs');
+
+let mainWindow
+//process.env.ELECTRON_RUN_AS_NODE = true;
+
+function createWindow () { //not being called
+//fixPath();
+//process.env.ELECTRON_RUN_AS_NODE = true
+  //console.log(process.env);
+
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+
+}
+
+app.on('ready', launchPM2); //only pm2 shall be launched
+
+/*
+var fpath = path.join(__dirname, '/node_modules/pm2/bin/pm2');
+const child = execFile(fpath, ['list'], (error, stdout, stderr) => {
+    if (error) {
+        console.error('stderr', stderr);
+        throw error;
+    }
+    console.log('stdout', stdout);
+});
+*/
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+function launchPM2() {
+  pm2.connect(function(err) { //start up pm2 god
+    if (err) {
+      console.log(err);
+      process.exit(2);
+    }
+  });
+  console.log("pm2 launched...");
+  //process.exit(2);
+}
